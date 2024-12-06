f99b465784e65e1ac2c8ad3868034534abed98c6
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2024-12-06 13:51:22 +0700

    Add database connection

--- scraper/enao-genre-scraper.py
+++ scraper/enao-genre-scraper.py
@@ -1,5 +1,8 @@
+import os
 import time
+import psycopg2
 import pandas as pd
+from dotenv import load_dotenv
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from selenium import webdriver
@@ -7,61 +10,91 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# Adjust this number based on your system's capabilities
-num_threads = int(input("How many threads do you want to use for the scraping? (Recommended: 4): ") or 4)
+load_dotenv()
 
 # Set up selenium driver
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chromedriver_path = 'C:\\Users\\Wildan Aziz\\OneDrive\\Documents\\Dev\\enoa-scraper\\chromedriver.exe'
+chromedriver_path = 'C:\\Users\\Wildan Aziz\\OneDrive\\Documents\\Dev\\enao-scraper\\chromedriver.exe'
 service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://everynoise.com/")
 
-# Record the start time
-start_time = time.time()
+# Set up db connection
+def init_db():
+    try:
+        conn = psycopg2.connect(
+            dbname=os.getenv("dbname"),
+            user=os.getenv("user"),
+            password=os.getenv("password"),
+            host=os.getenv("host"),
+            port=os.getenv("port")
+        )
+        print("Successfully connected to database.")
+        return conn
 
-# Scrape all genres + convert style attributes to DataFrame
-genres_elems = driver.find_elements(By.CLASS_NAME, "genre")
+    except:
+        print("Failed connecting to database.")
+        return None
 
-# Initialize an empty list for genre data
-genres_objs = []
+def main(conn):
+    # Record the start time
+    start_time = time.time()
 
-# Function to scrape genre data (this will run in parallel threads)
-def scrape_genre(genre):
-    genre_obj = {
-        "genre": genre.get_attribute("innerText"),
-        "preview_url": genre.get_attribute("preview_url"),
-        "preview_track": genre.get_attribute("title")
-    }
+    # Scrape all genres + convert style attributes to DataFrame
+    genres_elems = driver.find_elements(By.CLASS_NAME, "genre")
+
+    # Initialize an empty list for genre data
+    genres_objs = []
+
+    # Function to scrape genre data (this will run in parallel threads)
+    def scrape_genre(genre):
+        genre_obj = {
+            "genre": genre.get_attribute("innerText"),
+            "preview_url": genre.get_attribute("preview_url"),
+            "preview_track": genre.get_attribute("title")
+        }
+        
+        for style in genre.get_attribute("style").split(";")[:-1]:
+            [key, value] = style.split(":")
+            genre_obj[key.strip().replace("-", "_")] = value.strip()
+        
+        return genre_obj
+
+    # Create a ThreadPoolExecutor for concurrent scraping
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        # Use tqdm to display the progress
+        futures = [executor.submit(scrape_genre, genre) for genre in genres_elems]
+        
+        # Use tqdm to monitor progress
+        for future in tqdm(as_completed(futures), total=len(futures), desc="Scraping progress", unit="genre"):
+            genres_objs.append(future.result())
+
+    # Insert genres into database
+    if conn:
+        conn.cursor().executemany("""
+            INSERT INTO genre (genre_name, preview_url, preview_track, color, top_pixel, left_pixel, font_size) 
+            VALUES (%(genre)s, %(preview_url)s, %(preview_track)s, %(color)s, %(top)s, %(left)s, %(font_size)s);
+        """, genres_objs)
+        
+        conn.commit()
     
-    for style in genre.get_attribute("style").split(";")[:-1]:
-        [key, value] = style.split(":")
-        genre_obj[key.strip().replace("-", "_")] = value.strip()
-    
-    return genre_obj
+    # Convert the list of genre objects to a DataFrame
+    genres_df = pd.DataFrame(genres_objs)
 
-# Create a ThreadPoolExecutor for concurrent scraping
-with ThreadPoolExecutor(max_workers=num_threads) as executor:
-    # Use tqdm to display the progress
-    futures = [executor.submit(scrape_genre, genre) for genre in genres_elems]
-    
-    # Use tqdm to monitor progress
-    for future in tqdm(as_completed(futures), total=len(futures), desc="Scraping progress", unit="genre"):
-        genres_objs.append(future.result())
+    # Save data
+    genres_df.to_csv("C:\\Users\\Wildan Aziz\\OneDrive\\Documents\\Dev\\enao-scraper\\data\\enao-genres.csv", index=False)
 
-# Convert the list of genre objects to a DataFrame
-genres_df = pd.DataFrame(genres_objs)
+    # Record the end time
+    end_time = time.time()
 
-# Save data
-genres_df.to_csv("../data/enao-genres.csv", index=False)
+    # Calculate elapsed time
+    elapsed_time = end_time - start_time
+    print(f"Scraping process completed in {elapsed_time:.2f} seconds.")
 
-# Record the end time
-end_time = time.time()
+    # Clean up: close the browser
+    driver.quit()
 
-# Calculate elapsed time
-elapsed_time = end_time - start_time
-print(f"Scraping process completed in {elapsed_time:.2f} seconds.")
-
-# Clean up: close the browser
-driver.quit()
+if __name__ == "__main__":
+    conn = init_db()  
+    main(conn)
