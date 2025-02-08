9ff3189a5f1b5d96f50586fd4fcd20ef6aa03887
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2025-02-08 17:33:25 +0700

    Add small fixes

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ DOCKER_TAG="latest"
 FULL_IMAGE_NAME="$DOCKER_HUB_USER/$IMAGE_NAME:$DOCKER_TAG"
 
 echo "Building the Docker image..."
-docker build -t "$IMAGE_NAME" .
+docker build -t $IMAGE_NAME .
 
 echo "Tagging the Docker image as $FULL_IMAGE_NAME..."
-docker tag "$IMAGE_NAME" "$FULL_IMAGE_NAME"
+docker tag $IMAGE_NAME $FULL_IMAGE_NAME
 
 echo "Pushing the Docker image to Docker Hub..."
-docker push "$FULL_IMAGE_NAME"
+docker push $FULL_IMAGE_NAME
 
 echo "Docker image has been built, tagged, and pushed successfully."
--- deploy-resources.sh
+++ deploy-resources.sh
@@ -8,9 +8,9 @@ echo "Creating configmap..."
 kubectl create configmap postgres-config --from-env-file=.env
 
 echo "Creating PostgreSQL Persistent Volume and Persistent Volume Claim..."
-kubectl apply -f "$K8S_DIR/postgres-pv-pvc.yaml"
+kubectl apply -f $K8S_DIR/postgres-pv-pvc.yaml
 
 echo "Creating Deployment and Services..."
-kubectl apply -f "$K8S_DIR/main.yaml"
+kubectl apply -f $K8S_DIR/main.yaml
 
 echo "Deployment and Services have been created successfully."
